549fb2ea85cd02b61b9fcdcc20ce653164838874
miniseung <liontmdals@gmail.com>
2025-06-05 20:06:15 +0900

    docs: globalcss추가#6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Noto_Sans_KR } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const notoSansKR = Noto_Sans_KR({
+  variable: '--font-noto-sans-kr',
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -23,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="ko">
+      <body className={`${notoSansKR.variable} font-sans antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
 import { CustomButton } from '@/components/common/CustomButton';
 
 export default function Home() {
-
   const handletest = () => {
-    console.log("작동중")
-  }
+    console.log('작동중');
+  };
 
   return (
     <div>
       메인홈입니다.
-      
       {/* 예시 */}
-      <CustomButton label='경매 등록' size='lg' onClick={handletest}/>
+      <CustomButton label="경매 등록" size="lg" onClick={handletest} />
     </div>
   );
 }
