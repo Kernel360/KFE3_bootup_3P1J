b851e7340c023ec0733710e03605d618a99af51a
chhann <wjdcks2252@naver.com>
2025-06-06 15:20:03 +0900

    [feat] select 공통컴포넌트 구현 완료 #11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
 import { CustomButton } from '@/components/common/CustomButton';
 
 export default function Home() {
-  const handletest = () => {
-    console.log('작동중');
-  };
-
-  return (
-    <div>
-      메인홈입니다.
-      {/* 예시 */}
-      <CustomButton label="경매 등록" size="lg" onClick={handletest} />
-    </div>
-  );
+  return <div>메인홈입니다.</div>;
 }
